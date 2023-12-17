ec9cb1a686c26c05d92d1d404256d19d324bcd21
Kitae's Github <aqua0405@gmail.com>
2023-12-18 00:50:21 +0900

    Create ex01_insert_image.py

---
+++ basic/ch03_엑셀다루기/ex01_insert_image.py
@@ -0,0 +1,13 @@
+import openpyxl
+from openpyxl.drawing.image import Image
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+img_list = ["img1.png", "img2.png", "img3.png"]  # 이미지 리스트
+for i, image in enumerate(img_list, start=1):    # 반복 처리
+    img = Image(image)
+    img.width = 50
+    img.height = 50
+    sheet.add_image(img, f"A{i}")           # 시트에 이미지 추가
+
+wb.save("inserting_image.xlsx")
